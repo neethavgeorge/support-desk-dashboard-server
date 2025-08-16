0f197633b760fcb2a72d56e52e40ec7386989b47
Neetha V George <neetha.george@canstring.ai>
2025-08-16 22:04:58 +0530

    Fixed mongodB connection

--- src/server.js
+++ src/server.js
@@ -29,6 +29,15 @@ app.get("/", (req, res) => {
 
 
 
-app.listen(port,()=>{
-    console.log("Server running...")
-})
+// app.listen(port,()=>{
+//     console.log("Server running...")
+// })
+
+const startServer = async () => {
+  await connectDB();
+  app.listen(process.env.PORT || 5000, () => {
+    console.log(`🚀 Server running on port ${process.env.PORT || 5000}`);
+  });
+};
+
+startServer();
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,2 +1,3 @@
+require('dotenv').config();
 const jwt = require("jsonwebtoken");
 module.exports = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "7d" });
